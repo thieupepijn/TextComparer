985a2686eb49f9d3ea190347705feb08feba3278
thieupepijn <thieupepijn@hotmail.com>
2020-01-25 21:32:44 +0100

    first commit

---
+++ TextComparer/UtilDiffExtra.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace TextComparer
+{
+    public class UtilDiffExtra
+    {
+
+        public static void DiffCommonList2MarkedUpStrings(List<UtilDiff.commonOrDifferentThing> listDiffCommon, ref string markedUpString1, ref string markedUpString2)
+        {
+            StringBuilder sbMarkedUp1 = new StringBuilder();
+            StringBuilder sbMarkedUp2 = new StringBuilder();
+
+            foreach (UtilDiff.commonOrDifferentThing commonORdifferent in listDiffCommon)
+            {
+                if ((commonORdifferent.common != null) && (commonORdifferent.common.Count > 0))
+                {
+                    string rawstring = StringList2String(commonORdifferent.common);
+                    string bufMarkedUpString = PutStringInFont(rawstring, "green");
+                    sbMarkedUp1.Append(bufMarkedUpString);
+                    sbMarkedUp2.Append(bufMarkedUpString);
+                }
+                else
+                {
+                    if ((commonORdifferent.file1 != null) && (commonORdifferent.file1.Count > 0))
+                    {
+                        string rawstring = StringList2String(commonORdifferent.file1);
+                        string bufMarkedUpString = PutStringInFont(rawstring, "red");
+                        sbMarkedUp1.Append(bufMarkedUpString);
+                    }
+
+                    if ((commonORdifferent.file2 != null) && (commonORdifferent.file2.Count > 0))
+                    {
+                        string rawstring = StringList2String(commonORdifferent.file2);
+                        string bufMarkedUpString = PutStringInFont(rawstring, "red");
+                        sbMarkedUp2.Append(bufMarkedUpString);
+                    }
+                }
+
+            }
+
+            markedUpString1 = sbMarkedUp1.ToString();
+            markedUpString2 = sbMarkedUp2.ToString();
+        }
+
+        public static string[] WordsFromText(string inputString)
+        {
+            string[] wordsArray = inputString.Split(" ".ToCharArray());
+            List<string> listOut = wordsArray.ToList<string>();
+            listOut.RemoveAll(string.IsNullOrWhiteSpace);
+            return listOut.ToArray();
+        }
+
+
+        private static string StringList2String(List<string> listStrings)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (string stringetje in listStrings)
+            {
+                sb.Append(stringetje);
+                // sb.Append("&nbsp;");
+                sb.Append(" ");
+            }
+            return sb.ToString();
+        }
+
+        private static string PutStringInFont(string stringetje, string colorName)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<FONT COLOR=\"");
+            sb.Append(colorName);
+            sb.Append("\"");
+            sb.Append(">");
+            sb.Append(stringetje);
+            sb.Append("</FONT>");
+            return sb.ToString();
+        }
+
+        public static string ReplaceNewLines(string code)
+        {
+            code = code.Replace(System.Environment.NewLine, "<BR/>");
+            return code;
+        }
+
+    }
+}
---
+++ TextComparerConsole/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using TextComparer;
+
+namespace TextComparerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length < 4)
+            {
+                Console.WriteLine("Shows the difference of 2 strings with 2 html-pages");
+                Console.WriteLine("Usage: TextComparerConsole string1 string2 HTMLfilePath1 HTMLfilePath2");
+                return;
+            }
+
+            try
+            {
+                string text1 = args[0];
+                string text2 = args[1];
+
+                string htmlFilePath1 = args[2];
+                string htmlFilePath2 = args[3];
+
+                string[] textArray1 = UtilDiffExtra.WordsFromText(text1);
+                string[] textArray2 = UtilDiffExtra.WordsFromText(text2);
+
+                List<UtilDiff.commonOrDifferentThing> differenceList = UtilDiff.diff_comm(textArray1, textArray2);
+
+                string htmlString1 = string.Empty;
+                string htmlString2 = string.Empty;
+                UtilDiffExtra.DiffCommonList2MarkedUpStrings(differenceList, ref htmlString1, ref htmlString2);
+
+                WriteTextToFile(htmlString1, htmlFilePath1);
+                WriteTextToFile(htmlString2, htmlFilePath2);
+               
+                
+            }
+            catch
+            {
+
+            }
+        }
+
+        private static void WriteTextToFile(string text, string filePath)
+        {
+            using (StreamWriter writer = File.CreateText(filePath))
+            {
+                writer.Write(text);
+            }
+        }
+
+    }
+}
